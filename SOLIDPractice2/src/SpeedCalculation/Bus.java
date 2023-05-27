52e288de4eeef22cbf00ab7c387d374e5f8e356b
Alex <119449496+AlexandraPirog@users.noreply.github.com>
2023-05-27 13:04:29 +0300

    Add files via upload

---
+++ SOLIDPractice2/src/SpeedCalculation/Bus.java
@@ -0,0 +1,11 @@
+package SpeedCalculation;
+
+public class Bus extends Vehicle{
+    public Bus(int maxSpeed, String type) {
+        super(maxSpeed, type);
+    }
+    @Override
+    public double calculateAllowedSpeed() {
+        return super.maxSpeed * 0.6;
+    }
+}
